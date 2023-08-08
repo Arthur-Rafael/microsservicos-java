dafc15d3edf11081cd8144daa438b23c4c6cbec5
Arthur Rafael <arthurrsj123@gmail.com>
2023-08-08 11:33:03 -0300

    RestTemplate

--- hr-payroll/src/main/java/com/arthur/hrpayroll/servicies/PayrollService.java
+++ hr-payroll/src/main/java/com/arthur/hrpayroll/servicies/PayrollService.java
@@ -1,13 +1,30 @@
 package com.arthur.hrpayroll.servicies;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import com.arthur.hrpayroll.entities.Payment;
+import com.arthur.hrpayroll.entities.Worker;
 
 @Service
 public class PayrollService {
 
+	@Value("${hr-worker.host}")
+	private String workerHost;
+	
+	@Autowired
+	private RestTemplate restTemplate;
+	
 	public Payment getPayment(long workerId, int days) {
-		return new Payment("Bob", 200.0, days);
+		Map<String, String> uriVariables = new HashMap<>();
+		uriVariables.put("id", ""+workerId);
+		
+		Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariables);
+		return new Payment(worker.getName(), worker.getDailyIncome(), days);
 	}
 }
