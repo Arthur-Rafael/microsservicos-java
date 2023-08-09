05ccf8152d4c38515fe86c96152cd6c6ec629772
Arthur Rafael <arthurrsj123@gmail.com>
2023-08-09 11:24:19 -0300

    Hystrix fault tolerance

--- hr-worker/src/main/java/com/arthur/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/arthur/hrworker/resources/WorkerResource.java
@@ -2,6 +2,7 @@ package com.arthur.hrworker.resources;
 
 import java.util.List;
 
+import org.bouncycastle.crypto.RuntimeCryptoException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
